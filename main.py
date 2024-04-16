60fa935769a6f291f1867e8e6c7775268b3b66e0
DonatasNoreika <donoras@gmail.com>
2024-04-16 10:58:49 +0300

    Fixed problem with 0 ndigits

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ number = float(input("Enter a float number: "))
 
 ndig = int(input("Enter ndigits: "))
 
-print(f"Your rounded number is: {round(number, ndig)}")
+if ndig == 0:
+    print(f"Your rounded number is: {int(round(number, ndig))}")
+else:
+    print(f"Your rounded number is: {round(number, ndig)}")
